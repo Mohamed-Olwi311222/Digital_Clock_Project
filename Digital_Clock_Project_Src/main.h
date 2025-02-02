3973327d3eef8d7252436f7c16075c9ca6c4ed11
Mohamed Olwi <acemido2004@gmail.com>
2025-02-03 00:14:56 +0200

    Added a .h file fr the main.c

---
+++ Digital_Clock_Project_Src/main.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   application.h
+ * Author: Mohamed olwi
+ *
+ * Created on 03 October 2024, 05:25
+ */
+#ifndef APPLICATION_H
+#define	APPLICATION_H
+/*----------------------------Header Files------------------------------------*/
+#define __AVR_ATmega328P__                                                      /* MCU for the include dir*/
+#define F_CPU		                                         16000000UL		    /* MCU Clock */
+#include <avr/io.h>
+#include <util/delay.h>
+/*----------------------------Data Types--------------------------------------*/
+#endif	/* APPLICATION_H */
