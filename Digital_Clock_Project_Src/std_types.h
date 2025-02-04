a4f847f00d7814c3779a32b5e91ce61fc88c578f
Mohamed Olwi <acemido2004@gmail.com>
2025-02-04 04:07:10 +0200

    Added a standard types .h file to make every driver include it

---
+++ Digital_Clock_Project_Src/std_types.h
@@ -0,0 +1,37 @@
+/* 
+ * File:   std_types.h
+ * Author: Mohamed olwi
+ *
+ * Created: 2/2/2025 8:15:36 AM
+ */
+#ifndef MCAL_STD_TYPES_H
+#define	MCAL_STD_TYPES_H
+/*----------------------------Header Files------------------------------------*/
+#include <avr/io.h>
+#include <util/delay.h>
+/*----------------------------DataTypes---------------------------------------*/
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned long uint32;
+typedef char sint8;
+typedef short sint16;
+typedef long sint32;
+typedef uint8 Std_ReturnType;
+/*----------------------------Macros Declarations-----------------------------*/
+#define STD_HIGH        0x01    /* Physical state 5V or 3.3V */
+#define STD_LOW         0x00    /* Physical state 0V */
+
+
+#define STD_ON          0x01
+#define STD_OFF         0x00
+
+#define STD_ACTIVE      0x01    /* Logical state active */
+#define STD_IDLE        0x01    /* Logical state idle */
+
+#ifndef STATUSTYPEDEFINED
+#define STATUSTYPEDEFINED
+#define E_OK            (Std_ReturnType)0x00u
+#define E_NOT_OK        (Std_ReturnType)0x01u
+#endif
+
+#endif	/* MCAL_STD_TYPES_H */
