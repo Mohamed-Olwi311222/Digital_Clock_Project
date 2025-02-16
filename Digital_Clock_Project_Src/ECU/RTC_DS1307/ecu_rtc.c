d87cb743bc91c4421883c60e2e719f7748ebcbbf
Mohamed Olwi <acemido2004@gmail.com>
2025-02-16 02:40:29 +0200

    Made the rtc set/read time smaller by using helperfunctions instead to make the code easier to maintain and debugg

--- Digital_Clock_Project_Src/ECU/RTC_DS1307/ecu_rtc.c
+++ Digital_Clock_Project_Src/ECU/RTC_DS1307/ecu_rtc.c
@@ -5,6 +5,10 @@
  * Created: 10/2/2025 7:49:00 AM
  */
 #include "ecu_rtc.h"
+static Std_ReturnType inline rtc_set_day_time(uint8 *const time);
+static Std_ReturnType inline rtc_set_date(uint8 *const date);
+static Std_ReturnType inline rtc_read_day_time(uint8 *const buffer);
+static Std_ReturnType inline rtc_read_date(uint8 *const date);
 
 /**
  * @brief Initialize the RTC DS1307
@@ -14,52 +18,139 @@ void rtc_init(void)
 	i2c_init(I2C_STANDARD_MODE_100KHZ);
 }
 /**
- * @brief: initialize the I2C Comms with the RTC and read the RTC
- * @param hours hours to set
- * @param minutes minutes to set
- * @param seconds seconds to set
- * @param days days to set
- * @param months months to set
- * @param years years to set
+ * @brief: Write time to RTC using I2C
+ * @param time The time to write to the RTC
+ * @param time_len The length of time array (must be 6)
+ * @note time array must be of length 6 (seconds, minutes, seconds, days, months, years)
+ * @return E_OK if success otherwise E_NOT_OK
  */
-void rtc_set_time(uint8 hours, uint8 minutes, uint8 seconds, uint8 days, uint8 months, uint8 years)
+Std_ReturnType rtc_set_time(uint8 *const time, const uint8 time_len)
 {
-    seconds = seconds & 0x7F;  // Ensure CH bit (bit 7) is cleared
+    Std_ReturnType ret_val = E_OK;
 
-    i2c_master_write_buffer(RTC_ADDR, RTC_SECONDS_REG, seconds);
-    _delay_us(1000);
-    i2c_master_write_buffer(RTC_ADDR, RTC_MINUTES_REG, minutes);
-    _delay_us(1000);
-    i2c_master_write_buffer(RTC_ADDR, RTC_HOURS_REG, hours);
-    _delay_us(1000);
-    i2c_master_write_buffer(RTC_ADDR, RTC_DAYS_REG, days);
-    _delay_us(1000);
-    i2c_master_write_buffer(RTC_ADDR, RTC_MONTHS_REG, months);
-    _delay_us(1000);
-    i2c_master_write_buffer(RTC_ADDR, RTC_YEARS_REG, years);
+    if (NULL == time || 6 != time_len)
+    {
+        ret_val = E_NOT_OK;
+    }
+    else
+    {
+        /* Set time */
+        ret_val |= rtc_set_day_time(time);
+        ret_val |= rtc_set_date(time);
+    }
+    return (ret_val);
 }
 /**
- * @brief: Read the time and store them in addresses given
- * @param hours hours address to store into the read value
- * @param minutes minutes address to store into the read value
- * @param seconds seconds address to store into the read value
- * @param days days address to store into the read value
- * @param months months address to store into the read value
- * @param years years address to store into the read value
+ * @brief: Set the RTC date (Days, months, years)
+ * @param date The date to set
+ * @return E_OK if success otherwise E_NOT_OK
  */
-void rtc_read_time(uint8 *hours, uint8 *minutes, uint8 *seconds, uint8 *days, uint8 *months, uint8 *years)
+static Std_ReturnType inline rtc_set_date(uint8 *const date)
 {
-    i2c_master_read_data(RTC_ADDR, RTC_SECONDS_REG, seconds);
-    _delay_us(1000);
-    i2c_master_read_data(RTC_ADDR, RTC_MINUTES_REG, minutes);
-    _delay_us(1000);
-    i2c_master_read_data(RTC_ADDR, RTC_HOURS_REG, hours);
-    _delay_us(1000);
-    i2c_master_read_data(RTC_ADDR, RTC_DAYS_REG, days);
-    _delay_us(1000);
-    i2c_master_read_data(RTC_ADDR, RTC_MONTHS_REG, months);
-    _delay_us(1000);
-    i2c_master_read_data(RTC_ADDR, RTC_YEARS_REG, years);
+    Std_ReturnType ret_val = E_OK;
+    uint8 current_date_idx = RTC_DAYS_IDX;
+    uint8 current_reg_idx = RTC_DAYS_REG;
+
+    for (; current_date_idx <= RTC_YEARS_IDX; current_date_idx++)
+    {
+        ret_val = i2c_master_write_buffer(RTC_ADDR, current_reg_idx, date[current_date_idx]);
+        _delay_us(1000);
+        if (E_NOT_OK == ret_val)
+        {
+            break;
+        }
+        current_reg_idx++;
+    }
+    return (ret_val);
+}
+/**
+ * @brief: Set the RTC day time (Seconds, minutes, hours)
+ * @param time The time to set
+ * @return E_OK if success otherwise E_NOT_OK
+ */
+static Std_ReturnType inline rtc_set_day_time(uint8 *const time)
+{
+    Std_ReturnType ret_val = E_OK;
+    uint8 current_reg_idx = RTC_SECONDS_REG;
+
+    time[RTC_SECONDS_IDX] = time[RTC_SECONDS_IDX] & 0x7F;  // Ensure CH bit (bit 7) is cleared
+    for (; current_reg_idx < RTC_DAYS_IDX; current_reg_idx++)
+    {
+        ret_val = i2c_master_write_buffer(RTC_ADDR, current_reg_idx, time[current_reg_idx]);
+        _delay_us(1000);
+        if (E_NOT_OK == ret_val)
+        {
+            break;
+        }
+    }
+    return (ret_val);
+}
+/**
+ * @brief: Read time from RTC using I2C
+ * @param buffer The buffer to store the readen time
+ * @param buffer_len The length of buffer array (must be 6)
+ * @note time array must be of length 6 (seconds, minutes, seconds, days, months, years)
+ * @return E_OK if success otherwise E_NOT_OK
+ */
+Std_ReturnType rtc_read_time(uint8 *const buffer, const uint8 buffer_len)
+{
+    Std_ReturnType ret_val = E_OK;
+
+    if (NULL == buffer || 6 != buffer_len)
+    {
+        ret_val = E_NOT_OK;
+    }
+    else
+    {
+        /* Set time */
+        ret_val |= rtc_read_day_time(buffer);
+        ret_val |= rtc_read_date(buffer);
+    }
+    return (ret_val);
+}
+/**
+ * @brief: Read the RTC date (Days, months, years)
+ * @param buffer The buffer to store the readen date
+ * @return E_OK if success otherwise E_NOT_OK
+ */
+static Std_ReturnType inline rtc_read_date(uint8 *const buffer)
+{
+    Std_ReturnType ret_val = E_OK;
+    uint8 current_date_idx = RTC_DAYS_IDX;
+    uint8 current_reg_idx = RTC_DAYS_REG;
+
+    for (; current_date_idx <= RTC_YEARS_IDX; current_date_idx++)
+    {
+        ret_val = i2c_master_read_data(RTC_ADDR, current_reg_idx, &buffer[current_date_idx]);
+        _delay_us(1000);
+        if (E_NOT_OK == ret_val)
+        {
+            break;
+        }
+        current_reg_idx++;
+    }
+    return (ret_val);
+}
+/**
+ * @brief: Read the RTC day time (Seconds, minutes, hours)
+ * @param buffer The buffer to store the readen time
+ * @return E_OK if success otherwise E_NOT_OK
+ */
+static Std_ReturnType inline rtc_read_day_time(uint8 *const buffer)
+{
+    Std_ReturnType ret_val = E_OK;
+    uint8 current_reg_idx = RTC_SECONDS_REG;
+
+    for (; current_reg_idx < RTC_DAYS_IDX; current_reg_idx++)
+    {
+        ret_val = i2c_master_read_data(RTC_ADDR, current_reg_idx, &buffer[current_reg_idx]);
+        _delay_us(1000);
+        if (E_NOT_OK == ret_val)
+        {
+            break;
+        }
+    }
+    return (ret_val);
 }
 /**
  * @brief: Write the time in EEPROM
